06d2653cd5d8a9aabab45cdf6c81122888b5ce1e
Andrew DGalga <ex.spake@gmail.com>
2024-03-18 11:46:39 -0500

    init seats, todo fragment.

--- js/index.js
+++ js/index.js
@@ -2,12 +2,16 @@ const seatHost = document.querySelector('#theater--seats_host');
 const seatCount = 66;
 
 window.onload = () =>{
-    /*
+    
     for(let i = 0; i < seatCount; i++) {
-
+        const seat = document.createElement('button');
+        seat.classList.add('theater--seat');
+        seatHost.append(seat);
     }
+
+    /*
+    const seat = document.createElement('button');
+    seat.classList.add('theater--seat');
+    seatHost.append(seat);
     */
-   const seat = document.createElement('button');
-   seat.classList.add('theater-seat');
-   seatHost.append(seat);
 }
