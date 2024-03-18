d16f7ec56b54d556c113f5848a59b4b2a6e40f81
Andrew DGalga <ex.spake@gmail.com>
2024-03-18 11:29:26 -0500

    base colors, init button creation, stylesheet.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 const seatHost = document.querySelector('#theater--seats_host');
+const seatCount = 66;
+
+window.onload = () =>{
+    /*
+    for(let i = 0; i < seatCount; i++) {
+
+    }
+    */
+   const seat = document.createElement('button');
+   //seat.classList.add('');
+   seatHost.append(seat);
+}
