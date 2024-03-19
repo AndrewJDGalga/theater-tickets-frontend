36ed0af522db06074341c59dade0d75623014fa9
Andrew DGalga <ex.spake@gmail.com>
2024-03-19 16:08:21 -0500

    silly empty class to force child update for EM solution. Feels hacky, TODO find a different solution for future projects + test impact of user font size for scaling once elements all in place.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
 const seatHost = document.querySelector('#theater--seats_host');
+const zoomBtn = document.querySelector('#theater--zoom');
 
 const seatCount = 66;
 
-window.onload = () =>{
+const genSeats = ({numSeats, seatClass})=>{
     const seatsFrag = document.createDocumentFragment();
-    for(let i = 0; i < seatCount; i++) {
+    for(let i = 0; i < numSeats; i++) {
         const seat = document.createElement('button');
-        seat.classList.add('theater--seat');
+        seat.classList.add(seatClass);
         seatsFrag.append(seat);
     }
-    seatHost.append(seatsFrag);
+    return seatsFrag;
+}
+
+window.onload = () =>{
+    seatHost.append(genSeats({numSeats:seatCount, seatClass:'theater--seat'}));
+
+    zoomBtn.onclick = () => {
+        seatHost.classList.toggle('double-em');
+        for(let child of seatHost.children){
+            child.classList.toggle('one-em');
+        }
+    }
 }
