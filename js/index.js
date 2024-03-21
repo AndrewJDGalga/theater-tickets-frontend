50ce960e7c177afe18a04f3a1d39a290f20932b5
Andrew DGalga <ex.spake@gmail.com>
2024-03-21 16:25:19 -0500

    need seat data, rethinking 2for loops for easy 2D data.

--- js/index.js
+++ js/index.js
@@ -3,13 +3,28 @@ const zoomBtn = document.querySelector('#theater--zoom');
 
 const seatCount = 66;
 
+const seatCol = 11;
+const seatRow = 6;
+
+const rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G','H','I','J'];
+
 const genSeats = ({numSeats, seatClass})=>{
     const seatsFrag = document.createDocumentFragment();
+    for(let y = 0; y < seatRow; y++){
+        let col = rows[y];
+        for(let x = 0; x < seatCol; x++){
+            const seat = document.createElement('button');
+            seat.classList.add(seatClass);
+            seatsFrag.append(seat);
+        }
+    }
+    /*
     for(let i = 0; i < numSeats; i++) {
         const seat = document.createElement('button');
         seat.classList.add(seatClass);
+        //seat.dataset.row = 
         seatsFrag.append(seat);
-    }
+    }*/
     return seatsFrag;
 }
 
